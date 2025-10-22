d75a41281f74b5fb72d0ff1314e38e95bbd2bf1d
futaba <futaba4161999@icloud.com>
2025-10-22 22:08:18 +0900

    add テーブル作成

--- app/settings.py
+++ app/settings.py
@@ -20,6 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.accounts.apps.AccountsConfig',
+    'app.goals.apps.GoalsConfig',
+    'app.steps.apps.StepsConfig',
 ]
 
 MIDDLEWARE = [
@@ -37,7 +40,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,4 +112,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
