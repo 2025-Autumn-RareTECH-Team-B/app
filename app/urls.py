b7de6da8562c6d114401e7dbcc50561ed9e2ca22
NARU06120322 <junnarurei@gmail.com>
2025-11-21 23:48:40 +0900

    update:本番環境

--- app/urls.py
+++ app/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('health/', lambda request: HttpResponse("ok")),
     path('admin/', admin.site.urls),
     path('goals/', include(('app.goals.urls'))),
     path('', include('app.accounts.urls')),
