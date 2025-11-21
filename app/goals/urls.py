ac705b59f82255e2ffdcb26dccdf9d118d9ee2ee
seibow <airwhite2525@gmail.com>
2025-11-21 15:27:09 +0900

    add:人生マップリンクに準備中画像の表示

--- app/goals/urls.py
+++ app/goals/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("<int:goal_id>/edit/", views.goals_edit, name="goal_edit"), 
     path("<int:goal_id>/delete/", views.goals_delete, name="goal_delete"), 
     path("<int:goal_id>/complete/", views.complete_goal, name="complete_goal"),
+    path('life-map/', views.life_map, name="life_map"),
 ]
