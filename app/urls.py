535f510a3812d065c07531c1c82a61906375d662
NARU <133183051+NARU06120322@users.noreply.github.com>
2025-12-07 03:43:54 +0900

    Redirect root URL to login page

--- app/urls.py
+++ app/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),
     path('health/', lambda request: HttpResponse("ok")),
     path('admin/', admin.site.urls),
     path('goals/', include(('app.goals.urls'))),
